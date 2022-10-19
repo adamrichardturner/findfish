fe93b19051ed1aaeadaa8cffe4c7aba9f13c64b7
Adam Turner <hello@adamrichardturner.dev>
2022-10-19 23:15:29 +0100

    Added mui and other packages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import MiniDrawer from '../components/MiniDrawer/MiniDrawer'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <MiniDrawer />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
